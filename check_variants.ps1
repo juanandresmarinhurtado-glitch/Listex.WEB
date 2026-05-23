e91b444e1d92f9db1be2ac34ed750e51880467fe
juanandresmarinhurtado-glitch <juanandresmarinhurtado@gmail.com>
2026-05-23 14:06:37 -0400

    Delete check_variants.ps1

--- check_variants.ps1
+++
@@ -1,7 +0,0 @@
-$url = 'https://listex.odoo.com/web/content/16027/api_catalogo_listex.json'
-$data = Invoke-RestMethod -Uri $url
-$grouped = $data | Group-Object nombre | Where-Object { $_.Count -gt 1 } | Select-Object -First 2
-foreach ($g in $grouped) {
-    Write-Host "--- $($g.Name) ---"
-    $g.Group | Select-Object id_producto, variantes | ConvertTo-Json -Depth 10
-}
